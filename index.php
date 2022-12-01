9f33f6550f7856e72cf777922e06b930c7ca3c22
jabeddev <colourfullkite@gmail.com>
2022-12-01 09:47:34 +0600

    completed demo home

--- index.php
+++ index.php
@@ -1,13 +1,100 @@
 <?php  
     // document open
     include './partials/document-open.php'; 
-    // navbar
-    include './partials/navbar.php';
 ?>
-
-<h1>
-    Home page
-</h1>
+<section class="banner banner--demo">
+    <div class="overlay">
+         <div class="container text-center text-white">
+            <picture>
+                <img class="banner__logo"  loading="lazy" src="./images/logos/logo.webp" alt="logo-image">
+            </picture>
+            <h1 class="banner__title lh-lg">CharityPress</h1>
+            <h4 class="banner__sub-title lh-lg">CharityPress - Charity Fundraising HTML Template</h4>
+            <div class="banner__group-btn d-flex align-items-center gap-4 justify-content-center text-center">
+                <a class="banner__button-primary" href="#">
+                    <span>buy theme</span>
+                </a>
+                <a class="banner__button-secondary" href="#">
+                    <span>demos</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!--banner-end-->
+<section class="demo-section">
+    <div class="container">
+        <div class="section-heading--center">
+            <h2 class="section-heading__title">3 Unique Demos</h2>
+            <p class="section-heading__sub-title">CHOOSE YOUR LAYOUT</p>
+        </div>
+        <div class="row gy-5 demo-section__preview-container">
+            <div class="col-sm-6 col-md-4">
+                <div class="demo-section__card">
+                    <picture>
+                        <img class="w-100 h-auto" width="310" height="470" loading="lazy" src="./images/common/demo_1.webp" alt="preview-image">
+                    </picture>
+                    <div class="btn-holder">
+                        <a href="home-1.php">
+                            <span>INDEX VERSION 1</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+            <div class="demo-section__card">
+                    <picture>
+                        <img class="w-100 h-auto" width="310" height="470" loading="lazy" src="./images/common/demo_2.webp" alt="preview-image">
+                    </picture>
+                    <div class="btn-holder">
+                        <a href="home-2.php">
+                            <span>INDEX VERSION 2</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4">
+            <div class="demo-section__card">
+                    <picture>
+                        <img class="w-100 h-auto" width="310" height="470" loading="lazy" src="./images/common/demo_3.webp" alt="preview-image">
+                    </picture>
+                    <div class="btn-holder">
+                        <a href="home-3.php">
+                            <span>INDEX VERSION 3</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--demo-section end-->
+<section class="psd-section">
+<div class="container">
+        <div class="section-heading--center">
+            <h2 class="section-heading__title">PSD Included</h2>
+            <p class="section-heading__sub-title">MULTI-VARIATION</p>
+        </div>
+        <div class="row gy-3 psd-section__preview-container">
+            <div class="col">
+                <div class="psd-section__card">
+                    <picture>
+                        <img class="w-100 h-auto" width="1110" height="545" loading="lazy" src="./images/common/mock_psd.webp" alt="preview-image">
+                    </picture>
+                    <div class="btn-holder">
+                        <a href="purchase.php">
+                            <span>PURCHASE NOW</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+</section>
+<!--psd-section end-->
+<footer class="site-footer text-center text-white">
+Copyright by©
+<a href="https://xpeedstudio.com/html/charitypress"target="_blank">xpeedstudio</a>
+- 2018
+</footer>
 
 
 <?php include './partials/document.close.php' ?>
