22e705957a063a8e346c7ac34105e3963ebba71c
mollahdev <mollah.dev@gmail.com>
2022-12-04 10:13:08 +0600

    added fw and owl carousel

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+jQuery(function(){
+    'use strict';
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        autoplay: true,
+        autoplayHoverPause: true,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+    })
+})
--- partials/document-open.php
+++ partials/document-open.php
@@ -6,9 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CharityPress - Charity Fundraising HTML Template</title>
     <link rel="shortcut icon" href="images/icons/favicon.ico">
+    <!-- external css dependecies -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./css/app.css">
+    <!-- internal css dependencies -->
+    <link rel="stylesheet" href="./css/app.css"/>
+    <!-- internal js dependencies -->
+    <script src="./libs/js/jquery-3.6.1.min.js" defer></script>
+    <script src="./libs/js/owl.carousel.min.js" defer></script>
+    <script src="./js/scripts.js" defer></script>
 </head>
 <body>
