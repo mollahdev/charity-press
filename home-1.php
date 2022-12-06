a6853a90a1126cc8e9665cdd153817190183d661
mollahdev <mollah.dev@gmail.com>
2022-12-06 10:26:29 +0600

    added home page banner initial styles

--- home-1.php
+++ home-1.php
@@ -4,28 +4,58 @@
     include './partials/navbar.php';
 ?>
 
-<section class="home-banner home-banner--1">
+<section class="banner home-banner banner--1">
     <div class="owl-carousel">
-        <div class="item">
+        <div class="item home-banner__item-1">
             <div class="overlay">
                 <div class="container">
-                    <h1>one</h1>
-                </div>
-            </div>
+                    <h1 class="banner__title banner__title--home lh-lg">Hunger is stalking the globe</h1>
+                    <h4 class="banner__sub-title banner__sub-title--home lh-lg">Hundreds of thousands of children experiencing or witnessing assault</h4>
+                    <div class="banner__group-btn d-flex align-items-center gap-4 justify-content-center text-center">
+                        <a class="banner__button-primary banner__button-primary--home has-border" href="#">
+                            <span>join us now</span>
+                        </a>
+                        <a class="banner__button-secondary banner__button-secondary--home" href="#">
+                            <i class="fa-solid fa-heart"></i>
+                            <span>Donate Now</span>
+                        </a>
+                    </div><!--end .banner__group-btn-->
+                </div><!--end .container-->
+            </div> <!--end .overlay-->
         </div>
-        <div class="item">
+        <div class="item home-banner__item-2">
             <div class="overlay">
                 <div class="container">
-                    <h1>two</h1>
-                </div>
-            </div>
+                    <h1 class="banner__title banner__title--home lh-lg">Hunger is stalking the globe</h1>
+                    <h4 class="banner__sub-title banner__sub-title--home lh-lg">Hundreds of thousands of children experiencing or witnessing assault</h4>
+                    <div class="banner__group-btn d-flex align-items-center gap-4 justify-content-center text-center">
+                        <a class="banner__button-primary banner__button-primary--home has-border" href="#">
+                            <span>join us now</span>
+                        </a>
+                        <a class="banner__button-secondary banner__button-secondary--home" href="#">
+                            <i class="fa-solid fa-heart"></i>
+                            <span>Donate Now</span>
+                        </a>
+                    </div><!--end .banner__group-btn-->
+                </div><!--end .container-->
+            </div> <!--end .overlay-->
         </div>
-        <div class="item">
+        <div class="item home-banner__item-3">
             <div class="overlay">
                 <div class="container">
-                    <h1>three</h1>
-                </div>
-            </div>
+                    <h1 class="banner__title banner__title--home lh-lg">Hunger is stalking the globe</h1>
+                    <h4 class="banner__sub-title banner__sub-title--home lh-lg">Hundreds of thousands of children experiencing or witnessing assault</h4>
+                    <div class="banner__group-btn d-flex align-items-center gap-4 justify-content-center text-center">
+                        <a class="banner__button-primary banner__button-primary--home has-border" href="#">
+                            <span>join us now</span>
+                        </a>
+                        <a class="banner__button-secondary banner__button-secondary--home" href="#">
+                            <i class="fa-solid fa-heart"></i>
+                            <span>Donate Now</span>
+                        </a>
+                    </div><!--end .banner__group-btn-->
+                </div><!--end .container-->
+            </div> <!--end .overlay-->
         </div>
     </div>
 </section>
