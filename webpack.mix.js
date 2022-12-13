1069a9f377c939ac4463b79bd76082731a6bceff
mollahdev <mollah.dev@gmail.com>
2022-12-13 10:07:33 +0600

    cleaned branch

--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-// webpack.mix.js
-let mix = require('laravel-mix');
-mix.options({
-    // Don't perform any css url rewriting by default
-    processCssUrls: false,
-})
-mix.sass('scss/app.scss', 'css');
